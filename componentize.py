cb993e743053c394b7399cca1c9d6519424f99b1
Gonzalo Casas <casas@arch.ethz.ch>
2021-04-18 22:32:26 +0200

    moar tries

--- componentize.py
+++ componentize.py
@@ -6,11 +6,19 @@ import os
 import clr
 
 if __name__ == '__main__':
-    libdir = os.path.join(os.path.dirname(__file__), 'lib')
+    here = os.path.dirname(__file__)
+    libdir = os.path.join(here, 'lib')
     gh_io = os.path.join(libdir, 'GH_IO.dll')
+    print('Running on {}'.format(here))
+    print('Libs should be present under {}'.format(libdir))
 
     for f in glob.glob(os.path.join(libdir, '*.dll')):
         print('Found this:', f)
+    try:
+        clr.AddReferenceToFileAndPath(gh_io)
+        print('Managed to find by path')
+    except:
+        clr.AddReference('GH_IO')
+        print('Managed to find by ref name!')
 
-    clr.AddReferenceToFileAndPath(gh_io)
     print('Done!')
